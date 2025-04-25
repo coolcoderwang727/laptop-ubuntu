e617859c98251968b2968f58b8fd4afcba1142de
Remy Wang <wangremy727@gmail.com>
2025-04-25 02:49:25 -0400

    Update boot:

--- boot.sh
+++ boot.sh
@@ -1,3 +1,11 @@
 set -e
 
 echo "=> Omakub is for fresh Ubuntu 24.04+ installations only!"
+echo -e "\nBegin installation (or abort with ctrl+c)..."
+
+sudo apt-get update >/dev/null
+sudo apt-get install -y git >/dev/null
+
+echo "Cloning laptop-ubuntu..."
+rm -rf ~/.local/share/laptop-ubuntu
+git clone https://github.com/coolcoderwang727/laptop-ubuntu.git ~/.local/share/laptop-ubuntu >/dev/null
