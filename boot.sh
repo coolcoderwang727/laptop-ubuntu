3b6f1594353bd7622f7eb2bc118ad9728d5260ea
Remy Wang <wangremy727@gmail.com>
2025-04-25 03:05:32 -0400

    update

--- boot.sh
+++ boot.sh
@@ -9,3 +9,6 @@ sudo apt-get install -y git >/dev/null
 echo "Cloning laptop-ubuntu..."
 rm -rf ~/.local/share/laptop-ubuntu
 git clone git@github.com:coolcoderwang727/laptop-ubuntu.git ~/.local/share/laptop-ubuntu >/dev/null
+
+echo "Installation starting..."
+source ~/.local/share/laptop-ubuntu/install.sh
