4346f5e74382b94b345b1e8365def0ae69df2719
coolcoderwang727 <92461353+coolcoderwang727@users.noreply.github.com>
2025-04-25 02:57:24 -0400

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,6 +1,6 @@
 set -e
 
-echo "=> Omakub is for fresh Ubuntu 24.04+ installations only!"
+echo "=> laptop-ubuntu is for fresh Ubuntu 22.04 installations only!"
 echo -e "\nBegin installation (or abort with ctrl+c)..."
 
 sudo apt-get update >/dev/null
@@ -8,4 +8,4 @@ sudo apt-get install -y git >/dev/null
 
 echo "Cloning laptop-ubuntu..."
 rm -rf ~/.local/share/laptop-ubuntu
-git clone https://github.com/coolcoderwang727/laptop-ubuntu.git ~/.local/share/laptop-ubuntu >/dev/null
+git clone git@github.com:coolcoderwang727/laptop-ubuntu.git ~/.local/share/laptop-ubuntu >/dev/null
